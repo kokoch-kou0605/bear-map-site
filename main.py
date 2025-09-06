d615c09f15f509b68ce980bf88689d5ade16f656
kokoch <160324577+kokoch-kou0605@users.noreply.github.com>
2025-09-06 20:07:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -121,5 +121,5 @@ def delete_location(location_id):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Render が指定するPORTを優先
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
